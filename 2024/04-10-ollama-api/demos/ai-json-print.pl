e9dfe90a6964ad58d714bed55fca7fc552b96b56
Wildcard Wizard <2887249+w1ldc4rd-w1z4rd@users.noreply.github.com>
2024-04-10 23:36:45 -0400

    added ai JSON print

---
+++ 2024/04-10-ollama-api/demos/ai-json-print.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl -n
+
+# prints response only from ollama
+
+use JSON::XS;
+use feature qw|say|;
+use Data::Dumper;
+
+$|++;
+
+my $data = decode_json $_;
+
+print $data->{response};
+
+__END__
+# demo request
+
+curl -sSL -N -X POST http://localhost:11434/api/generate \
+-H "Content-Type: application/json" \
+-d '{"model": "Mistral", "prompt": "[INST] Generate a valid JSON object from the following data: name: John, lastname: Smith, address: #1 Samuel St. [/INST]"}' | perl ./ai-json-print.pl
+
+# output
+{
+	"name": "John",
+	 "lastname": "Smith",
+	 "address":
+	 {
+	   "streetAddress": "#1 Samuel St."
+	 }
+}
